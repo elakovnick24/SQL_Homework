2c119be886c95dfd9b12a41ea1e62be20553633d
Nikita Elakov <89816960858@mail.ru>
2020-09-10 20:32:19 +0300

    add test

--- src/test/java/ru/netology/mode/DbInteract.java
+++ src/test/java/ru/netology/mode/DbInteract.java
@@ -1,4 +1,26 @@
 package ru.netology.mode;
 
+
+import com.github.javafaker.Faker;
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DbInteract {
+    @BeforeEach
+    void setUp() throws SQLException {
+        val faker = new Faker();
+        val dataSQL = "INSERT INTO users(login, password) VALUES (?, ?);";
+        try (
+                val conn = DriverManager.getConnection(
+                        "jdbc:mysql://192.168.99.100:3306/app", "app", "pass" );
+                val dataStmt = conn.prepareStatement(dataSQL); ){
+            dataStmt.setString(1, faker.name().username()); dataStmt.setString(2, "password"); dataStmt.executeUpdate();
+            dataStmt.setString(1, faker.name().username()); dataStmt.setString(2, "password"); dataStmt.executeUpdate();
+        } }
 }
